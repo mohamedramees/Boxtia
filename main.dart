86caf204a19965822694f822a0e7f30bfe4820fc
Mohamed Ramees TK <112646789+mohamedramees@users.noreply.github.com>
2024-07-08 20:52:39 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,39 @@
+import 'package:boxtia_inventory/Model/DB_Model.dart';
+import 'package:boxtia_inventory/Screens/Splash_Sreen.dart';
+
+
+import 'package:flutter/material.dart';
+// import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+const saveKeyName = 'UserLoggedIn';
+Future<void> main() async {
+  await Hive.initFlutter();
+  if (
+    !Hive.isAdapterRegistered(userModelAdapter().typeId)
+    // &&
+    // !Hive.isAdapterRegistered(itemModelAdapter().typeId)
+    ) {
+    Hive.registerAdapter(userModelAdapter());
+    // Hive.registerAdapter(itemModelAdapter());
+  }
+  await Hive.openBox<bool>('userBox');
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Boxtia',
+      theme: ThemeData(
+        colorScheme: const ColorScheme.light(brightness: Brightness.light)
+            .copyWith(primary: Colors.blue),
+      ),
+      home: const Splash_Screen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
