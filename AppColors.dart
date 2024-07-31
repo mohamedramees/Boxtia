1b732b071741989e3a0a15e93ab46941de6b21df
Mohamed Ramees TK <112646789+mohamedramees@users.noreply.github.com>
2024-07-31 18:18:09 +0530

    Add files via upload

--- AppColors.dart
+++ AppColors.dart
@@ -4,9 +4,10 @@ class AppColor {
   static const Color appBar = Color(0xFF003366);
   static const Color bottomBar = Color(0xFF003366);
   static const Color floating = Color(0xFF003366);
-  static const Color scaffold = Color.fromARGB(255, 150, 191, 223);
-  static const Color safeArea = Color.fromARGB(255, 155, 205, 243);
+  static const Color scaffold = Colors.transparent;
   static const Color textFormBorder = Color(0xFF003366);
   static const Color white = Colors.white;
   static const Color lGreenAc = Colors.lightGreenAccent;
+  static const Color black = Colors.black;
+  static const Color itemName = Color(0xFF003366);
 }
