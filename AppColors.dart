079f9d8d5053aa833be1f5a79b3b7c4fee1f5af6
Mohamed Ramees TK <112646789+mohamedramees@users.noreply.github.com>
2024-08-08 17:57:51 +0530

    Add files via upload

--- AppColors.dart
+++ AppColors.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
 
 class AppColor {
-  static const Color appBar = Color(0xFF003366);
-  static const Color bottomBar = Color(0xFF003366);
-  static const Color floating = Color(0xFF003366);
+  static const Color darkBlue = Color(0xFF003366);
   static const Color scaffold = Colors.transparent;
-  static const Color textFormBorder = Color(0xFF003366);
   static const Color white = Colors.white;
   static const Color lGreenAc = Colors.lightGreenAccent;
   static const Color black = Colors.black;
-  static const Color itemName = Color(0xFF003366);
+  static const Color blue = Colors.blue;
+  static const Color collapsedBackgroundColor = Color.fromARGB(217, 255, 255, 255);
+  static const Color reportListAmount = Color.fromARGB(255, 0, 107, 194);
 }
